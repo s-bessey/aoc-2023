845cbbedddf7eca521ce6573d56f5e6821017892
Sam Bessey <sam_bessey@brown.edu>
2023-12-01 16:17:32 -0500

    add linting stuff

--- day1/day1.js
+++ day1/day1.js
@@ -1,17 +1,16 @@
-import {read} from "../commonFiles/prepData.js"
+import { read } from '../commonFiles/prepData.js'
 
-const doc = read("day1/input.txt")
-
+const doc = read('day1/input.txt')
 
 // part 1
-const p1 = doc.map((val) => val.replace(/[^0-9]+/g, ""))
+const p1 = doc.map((val) => val.replace(/[^0-9]+/g, ''))
 
-const value = p1.map((val) => parseInt(val[0] + val[val.length - 1])).reduce((a,b) => a + b)
+const value = p1.map((val) => parseInt(val[0] + val[val.length - 1])).reduce((a, b) => a + b)
 
-console.log("Part 1: " + value)
+console.log('Part 1: ' + value)
 
 // part 2
-const p2 = doc.map((val) => val.replace(/one/g, "o1e").replace(/two/g, "t2o").replace(/three/g, "t3e").replace(/four/g, "f4r").replace(/five/g, "f5e").replace(/six/g, "s6x").replace(/seven/g, "s7n").replace(/eight/g, "e8t").replace(/nine/g, "n9e").replace(/[^0-9]+/g, ""))
-console.log("Part 2: " + p2.map((val) => parseInt(val[0] + val[val.length - 1])).reduce((a,b) => a + b))
+const p2 = doc.map((val) => val.replace(/one/g, 'o1e').replace(/two/g, 't2o').replace(/three/g, 't3e').replace(/four/g, 'f4r').replace(/five/g, 'f5e').replace(/six/g, 's6x').replace(/seven/g, 's7n').replace(/eight/g, 'e8t').replace(/nine/g, 'n9e').replace(/[^0-9]+/g, ''))
+console.log('Part 2: ' + p2.map((val) => parseInt(val[0] + val[val.length - 1])).reduce((a, b) => a + b))
 
 // 54978 is too low?
